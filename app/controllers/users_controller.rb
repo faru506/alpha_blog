7f05620ac0dc3f07c75af865f186e83f38bdce72
Farooq Shaik <shaikfarooq2020@gmail.com>
2022-03-16 20:09:33 +0530

    User Profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,10 @@
 class UsersController < ApplicationController
+
+  def show
+    @user = User.find(params[:id])
+    @articles = @user.articles
+  end
+
   def new
     @user = User.new
   end
@@ -10,10 +16,10 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
-      flash[:notice] = "Your account information was successfully updated."
+      flash[:notice] = "Your account information was successfully updated"
       redirect_to articles_path
     else
-      render "edit"
+      render 'edit'
     end
   end
 
